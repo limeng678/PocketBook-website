47e2fb5bc191ec85551d7076ed231671410b3b8f
lm <710450868@qq.com>
2020-07-26 10:12:04 +0800

    deploy

---
+++ precache-manifest.183f7eda5933aef1b6797f3ff678ace1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11c1d34c15d014ef4e69",
+    "url": "/pocketbook-website/css/app.0dfddf9c.css"
+  },
+  {
+    "revision": "6d5ff77f8f1c75928ef414a32ddd4390",
+    "url": "/pocketbook-website/index.html"
+  },
+  {
+    "revision": "11c1d34c15d014ef4e69",
+    "url": "/pocketbook-website/js/app.95ea9dc1.js"
+  },
+  {
+    "revision": "d46813643c28019220cb",
+    "url": "/pocketbook-website/js/chunk-vendors.b64a474c.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/pocketbook-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pocketbook-website/robots.txt"
+  }
+]);
